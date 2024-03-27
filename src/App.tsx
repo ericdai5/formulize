71f20acae9c79bc0e945d43d84fa794a8f3e41d2
Jeff Tao <jatao88@gmail.com>
2024-03-27 15:15:42 -0400

    Load & run MathJax

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import "mathjax";
+
+// import { mathjax } from "mathjax-full/ts/mathjax.ts";
+// import { TeX } from "mathjax-full/ts/input/tex.ts";
+// import { SVG } from "mathjax-full/ts/output/svg.ts";
+// import { CHTML } from "mathjax-full/ts/output/chtml.ts";
+// import { liteAdaptor } from "mathjax-full/ts/adaptors/liteAdaptor.ts";
+// import { RegisterHTMLHandler } from "mathjax-full/ts/handlers/html.ts";
+// import { AllPackages } from "mathjax-full/js/input/tex/AllPackages.js";
 
 function App() {
   const [count, setCount] = useState(1);
+  const [rendered, setRendered] = useState(false);
+  useEffect(() => {
+    if (!rendered) {
+      const tree = MathJax.tex2mml("a^2 + b^2 = c^2", {});
+      const treeNode = new DOMParser().parseFromString(tree, "text/xml");
+      console.log(treeNode);
+      const node = MathJax.tex2svg("a^2 + b^2 = c^2", {});
+      console.log(node);
+      document.body.appendChild(node);
+      setRendered(true);
+    }
+  }, [rendered, setRendered]);
+
+  // const html = mathjax.document(document, { InputJax: tex, OutputJax: chtml });
+  // const adaptor = liteAdaptor();;
+  // const handler = RegisterHTMLHandler(adaptor);;
+
+  // const tex = new TeX({ packages: ["base", "ams"] });;
+  // const svg = new SVG({ fontCache: "local" });;
+  // const chtml = new CHTML({;
+  //   fontURL:;
+  //     "https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2",;
+  // });;
+  // const node = html.convert("a^2 + b^2 = c^2", {
+  //   display: true,
+  //   // em: argv.em,
+  //   // ex: argv.ex,
+  //   // containerWidth: argv.width
+  // });
+  // console.log(adaptor.outerHTML(node));
 
   return (
     <>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+(window as any).MathJax = {
+  loader: {
+    load: ["input/tex", "output/svg", "[tex]/require"],
+  },
+  startup: {
+    pageReady: () => {
+      // We have to wait until MathJax is available before starting React
+      ReactDOM.createRoot(document.getElementById("root")!).render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      );
+    },
+  },
+};
+
+// Loading MathJax will eventually trigger the React render
+const script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/startup.js";
+script.async = true;
+document.head.appendChild(script);
