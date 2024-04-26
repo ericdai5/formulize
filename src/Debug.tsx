c4753733ab1a0b292ce98b5460efeee0dde5bbeb
Jeff Tao <jatao88@gmail.com>
2024-04-26 16:34:45 -0400

    Better Debug performance

--- src/Debug.tsx
+++ src/Debug.tsx
@@ -28,52 +28,70 @@ export const Debug = observer(() => {
         flex-direction: column;
       `}
     >
-      <pre>Targets:</pre>
-      <table
-        css={css`
-          font-family: monospace;
-        `}
-      >
-        <thead
-          css={css`
-            font-weight: bold;
-          `}
-        >
-          <tr>
-            <CoordinateCell>Id</CoordinateCell>
-            <CoordinateCell>Left</CoordinateCell>
-            <CoordinateCell>Top</CoordinateCell>
-            <CoordinateCell>Width</CoordinateCell>
-            <CoordinateCell>Height</CoordinateCell>
-          </tr>
-        </thead>
-        <tbody>
-          {Array.from(selectionStore.targets.values()).map((target) => (
-            <tr key={target.id}>
-              <CoordinateCell>{target.id}</CoordinateCell>
-              <CoordinateCell>{formatCoordinate(target.left)}</CoordinateCell>
-              <CoordinateCell>{formatCoordinate(target.top)}</CoordinateCell>
-              <CoordinateCell>{formatCoordinate(target.width)}</CoordinateCell>
-              <CoordinateCell>{formatCoordinate(target.height)}</CoordinateCell>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <pre>Selection rect:</pre>
-      {selectionStore.selectionRect ? (
-        <>
-          <pre>{"x1: " + selectionStore.selectionRect.x1}</pre>
-          <pre>{"y1: " + selectionStore.selectionRect.y1}</pre>
-          <pre>{"x2: " + selectionStore.selectionRect.x2}</pre>
-          <pre>{"y2: " + selectionStore.selectionRect.y2}</pre>
-        </>
-      ) : (
-        <pre>None</pre>
-      )}
-      <pre>Drag selection:</pre>
-      <pre>{Array.from(selectionStore.currentlyDragged).join(",\n")}</pre>
-      <pre>Selection:</pre>
-      <pre>{selectionStore.selected.join(",\n")}</pre>
+      <Targets />
+      <Drag />
+      <Selected />
     </div>
   );
 });
+
+const Targets = observer(() => (
+  <>
+    <pre>Targets:</pre>
+    <table
+      css={css`
+        font-family: monospace;
+      `}
+    >
+      <thead
+        css={css`
+          font-weight: bold;
+        `}
+      >
+        <tr>
+          <CoordinateCell>Id</CoordinateCell>
+          <CoordinateCell>Left</CoordinateCell>
+          <CoordinateCell>Top</CoordinateCell>
+          <CoordinateCell>Width</CoordinateCell>
+          <CoordinateCell>Height</CoordinateCell>
+        </tr>
+      </thead>
+      <tbody>
+        {Array.from(selectionStore.targets.values()).map((target) => (
+          <tr key={target.id}>
+            <CoordinateCell>{target.id}</CoordinateCell>
+            <CoordinateCell>{formatCoordinate(target.left)}</CoordinateCell>
+            <CoordinateCell>{formatCoordinate(target.top)}</CoordinateCell>
+            <CoordinateCell>{formatCoordinate(target.width)}</CoordinateCell>
+            <CoordinateCell>{formatCoordinate(target.height)}</CoordinateCell>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </>
+));
+
+const Drag = observer(() => (
+  <>
+    <pre>Selection rect:</pre>
+    {selectionStore.selectionRect ? (
+      <>
+        <pre>{"x1: " + selectionStore.selectionRect.x1}</pre>
+        <pre>{"y1: " + selectionStore.selectionRect.y1}</pre>
+        <pre>{"x2: " + selectionStore.selectionRect.x2}</pre>
+        <pre>{"y2: " + selectionStore.selectionRect.y2}</pre>
+      </>
+    ) : (
+      <pre>None</pre>
+    )}
+    <pre>Drag selection:</pre>
+    <pre>{Array.from(selectionStore.currentlyDragged).join(",\n")}</pre>
+  </>
+));
+
+const Selected = observer(() => (
+  <>
+    <pre>Selection:</pre>
+    <pre>{selectionStore.selected.join(",\n")}</pre>
+  </>
+));
