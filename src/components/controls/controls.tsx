4e8031dc7812cf0c69c4bde1c0039a1c7bb1ecbc
Eric Dai <ericdai2000@gmail.com>
2025-11-14 21:13:32 -0500

    Fix array control naming

--- src/components/controls/controls.tsx
+++ src/components/controls/controls.tsx
@@ -2,7 +2,7 @@ import { observer } from "mobx-react-lite";
 
 import { IControls, ISliderControl, IArrayControl, ISetControl, ICheckboxControl } from "../../types/control";
 import Slider from "./slider";
-import Array from "./array";
+import ArrayControl from "./array";
 import SetControl from "./set";
 import CheckboxControl from "./checkbox";
 
@@ -23,7 +23,7 @@ const ControlPanel = observer(({ controls }: ControlPanelProps) => {
         return <Slider key={key} control={control as ISliderControl} />;
 
       case "array":
-        return <Array key={key} control={control as IArrayControl} />;
+        return <ArrayControl key={key} control={control as IArrayControl} />;
 
       case "set":
         return <SetControl key={key} control={control as ISetControl} />;
