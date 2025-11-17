302d840ce165bdcec5a2e318288db8f28402cb6b
Eric Dai <ericdai2000@gmail.com>
2025-11-16 20:56:30 -0500

    Add Fitts Law example

---
+++ src/examples/fittsLaw.ts
@@ -0,0 +1,60 @@
+export const fittsLaw = `const config = {
+  formulas: [
+    {
+      formulaId: "fitts-law",
+      latex: "T = a + b \\\\log \\\\left( \\\\frac{2D}{W} \\\\right)",
+      expression: "{T} = {a} + {b} * log((2 * {D}) / {W})"
+    }
+  ],
+  variables: {
+    T: {
+      type: "dependent",
+      precision: 2,
+      name: "Time"
+    },
+    a: {
+      type: "constant",
+      value: 0.1,
+      range: [0.1, 10],
+      step: 0.1,
+    },
+    b: {
+      type: "constant",
+      value: 0.3,
+      range: [0, 10],
+      step: 0.1,
+    },
+    D: {
+      type: "input",
+      value: 50,
+      range: [0, 200],
+      step: 1,
+      name: "Distance"
+    },
+    W: {
+      type: "input",
+      value: 10,
+      step: 1,
+      range: [1, 500],
+      name: "Width"
+    }
+  },
+  computation: {
+    engine: "symbolic-algebra"
+  },
+  visualizations: [{
+    type: "plot2d",
+    xAxisVar: "D",
+    xRange: [0, 200],
+    yAxisVar: "T",
+    yRange: [0, 2],
+    lines: [{ name: "line" }],
+  }, {
+    type: "plot2d",
+    xAxisVar: "W",
+    xRange: [0, 200],
+    yAxisVar: "T",
+    yRange: [0, 2],
+    lines: [{ name: "line" }],
+  }]
+};`;
