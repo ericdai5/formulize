d5e48276109eec08483711ab8f448f4ae1dc2f37
Eric Dai <ericdai2000@gmail.com>
2026-01-26 16:55:48 -0800

    Add react routes for examples in the playground

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import * as acorn from "acorn";
 
@@ -76,7 +77,9 @@ const loadMathJax = () => {
 loadMathJax().then(() => {
   ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </React.StrictMode>
   );
 });
